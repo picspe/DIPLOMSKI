2729a4976c847586579f7db2bc93073aad405a0e
pepic <srpepson@gmail.com>
2018-03-04 00:54:39 +0100

    pom.xml fix

--- target/classes/public/app/home/controller.js
+++ target/classes/public/app/home/controller.js
@@ -38,7 +38,7 @@ angular.module('home.controllers', [])
                 'receiverMail': 'test.test1@sparkmail.com',
                 'cc': '',
                 'subject': 'test1'
-            },{
+            }, {
                 'id': 1,
                 'message': 'hello2',
                 'date': new Date(),
@@ -46,7 +46,7 @@ angular.module('home.controllers', [])
                 'receiverMail': 'test.test2@sparkmail.com',
                 'cc': '',
                 'subject': 'test2'
-            },{
+            }, {
                 'id': 1,
                 'message': 'hello3',
                 'date': new Date(),
@@ -54,7 +54,7 @@ angular.module('home.controllers', [])
                 'receiverMail': 'test.3@sparkmail.com',
                 'cc': '',
                 'subject': 'tes3'
-            },{
+            }, {
                 'id': 1,
                 'message': 'hello4',
                 'date': new Date(),
@@ -64,99 +64,112 @@ angular.module('home.controllers', [])
                 'subject': 'test4'
             }];
 
-            $scope.sendNewToggle = function() {
+            $scope.sendNewToggle = function () {
                 $('.card').addClass('hidden');
                 $('#inboxnav').addClass('hidden');
                 $('#outboxnav').addClass('hidden');
                 $('.new-mail').removeClass('hidden');
             };
 
-            $scope.inboxToggle = function() {
+            $scope.inboxToggle = function () {
                 $('.card').addClass('hidden');
                 $('#outboxnav').addClass('hidden');
                 $('#inboxnav').removeClass('hidden');
-                homeService.getInbox().then(function(response) {
+                homeService.getInbox().then(function (response) {
                     $scope.messages = response.data;
                 });
             };
 
-            $scope.outboxToggle = function() {
+            $scope.outboxToggle = function () {
                 $('.card').addClass('hidden');
                 $('#inboxnav').addClass('hidden');
                 $('#outboxnav').removeClass('hidden');
-                homeService.getOutbox().then(function(response) {
+                homeService.getOutbox().then(function (response) {
                     $scope.messages = response.data;
                 });
             };
 
-            $scope.openInboxMail = function(message) {
+            $scope.openInboxMail = function (message) {
                 $('.card').addClass('hidden');
                 $scope.inboxMail = message;
                 $('.inbox').removeClass('hidden');
             };
 
-            $scope.openOutboxMail = function(message) {
+            $scope.openOutboxMail = function (message) {
                 $('.card').addClass('hidden');
                 $scope.outboxMail = message;
                 $('.outbox').removeClass('hidden');
             };
 
-            $scope.send = function(mail) {
-                if(mail.cc || mail.cc !== "") {
+            $scope.send = function (mail) {
+                if (mail.cc || mail.cc !== "") {
                     var receivers = mail.cc.split(';');
                 } else {
                     receivers = [mail.receiverMail];
                 }
-                for(var i in receivers) {
+                for (var i in receivers) {
                     mail.receiverMail = receivers[i];
-                    homeService.send(mail).then(function(response) {
+                    homeService.send(mail).then(function (response) {
                         $scope.messages = response.data;
                     })
                 }
             };
 
-            $('#toBox').on( "keydown", function( event ) {
-                homeService.getTopTen(event.target.value).then(function(response) {
-                    $scope.userList = response.data;
-                })
-                if ( event.keyCode === $.ui.keyCode.TAB &&
-                    $( this ).autocomplete( "instance" ).menu.active ) {
-                    event.preventDefault();
-                }
-            })
-                .autocomplete({
-                    minLength: 0,
-                    source: function( request, response ) {
-                        // delegate back to autocomplete, but extract the last term
-                        response( $.ui.autocomplete.filter(
-                            $scope.userList , request.term.split(',').pop() ) );
-                    },
-                    focus: function() {
-                        // prevent value inserted on focus
-                        return false;
-                    },
-                    select: function( event, ui ) {
-                        var terms = this.value.split(',');
-                        // remove the current input
-                        terms.pop();
-                        // add the selected item
-                        terms.push( ui.item.value );
-                        // add placeholder to get the comma-and-space at the end
-                        terms.push( "" );
-                        this.value = terms.join( ", " );
-                        return false;
-                    }
-                });
+            function split( val ) {
+                return val.split( /,\s*/ );
+            }
+            function extractLast( term ) {
+                return split( term ).pop();
+            }
 
-            $scope.forward = function(mail) {
-                $scope.newMail = Object.assign({},mail);
+            $('.email-input').on("keydown", function (event) {
+
+                    if (event.keyCode === $.ui.keyCode.TAB &&
+                        $(this).autocomplete("instance").menu.active) {
+                        event.preventDefault();
+                    }
+
+
+            }).autocomplete({
+                minLength: 0,
+                source: function (request, response) {
+                    var searchTerm = request.term.split(',');
+                    searchTerm = searchTerm[searchTerm.length-1].replace(" ","");
+                    homeService.getTopTen(searchTerm).then(function (res) {
+                        $scope.userList = res.data.map(function(obj) {
+                            return obj.email;
+                        });
+                        // delegate back to autocomplete, but extract the last term
+                        response($.ui.autocomplete.filter(
+                            $scope.userList, searchTerm));
+                    });
+                },
+                focus: function () {
+                    // prevent value inserted on focus
+                    return false;
+                },
+                select: function (event, ui) {
+                    var terms = this.value.split(',');
+                    // remove the current input
+                    terms.pop();
+                    // add the selected item
+                    terms.push(ui.item.value);
+                    // add placeholder to get the comma-and-space at the end
+                    terms.push("");
+                    this.value = terms.join(", ");
+                    return false;
+                }
+            });
+
+            $scope.forward = function (mail) {
+                $scope.newMail = Object.assign({}, mail);
                 $scope.newMail.subject = "FW: " + $scope.newMail.subject;
                 $scope.newMail.cc = "Cc: " + $scope.newMail.cc + "\n";
                 $scope.newMail.message = "Forwarder from: " + mail.senderMail + "\n" + $scope.newMail.message;
                 $scope.sendNewToggle();
             };
 
-            $scope.reply = function(mail) {
+            $scope.reply = function (mail) {
 
             };
         }]
